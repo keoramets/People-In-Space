099bd88c1f43b68d11000ed6a722e75786e510b6
Nyasha <48386835+keoramets@users.noreply.github.com>
2022-03-06 14:09:43 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,51 +4,61 @@ const header = document.querySelector('header');
 const peopleList = document.getElementById('people');
 const btn = document.querySelector('button');
 
-function getProfiles(json) {
-    const number = json.number;
-  const profiles = json.people.map( person => {
-    const craft =  person.craft;
-    return fetch(wikiUrl + person.name)
-      .then( response => response.json() )
-      .then( profile => {
-        let newObj = {...profile, craft, number};
-        return newObj
-      } )
-      .catch( err => console.log(Error('Error fetching Wiki: ')) )   
-  }); 
+// Handle all fetch requests
+async function getJSON(url) {
+  try {
+    const response =  await fetch(url);
+    return await response.json();
+  } catch (error) {
+    throw error;
+  }
+} 
+
+async function getPeopleInSpace(url) {
+
+  const peopleJSON = await getJSON(url);
+
+  const astrosNumber = peopleJSON.number;
+
+  const profiles = peopleJSON.people.map( async (person) => {
+    const craft = person.craft;
+    const profileJSON = await getJSON(wikiUrl + person.name);
+
+    return{ ...profileJSON, craft, astrosNumber }
+  });
   return Promise.all(profiles);
 }
 
+
 function generateHTML(data) {
   data.map(person => {
+
     header.innerHTML = `
     <h1>People Currently in Space</h1>
-    <h2>${person.number}</h2>
+    <h2>${person.astrosNumber}</h2>
     `;
 
     const section = document.createElement('section');
-  peopleList.appendChild(section);
-    section.innerHTML = `
-      <img src=${person.thumbnail.source}>
-      <span>Spacecraft: ${person.craft}</span>
-      <h3>${person.title}</h3>
-      <p>${person.description}</p>
-      <p>${person.extract}</p>
-    `;
+    peopleList.appendChild(section);
+      section.innerHTML = `
+        <img src=${person.thumbnail.source}>
+        <span>Spacecraft: ${person.craft}</span>
+        <h3>${person.title}</h3>
+        <p>${person.description}</p>
+        <p>${person.extract}</p>
+      `;
+
   });
 }
 
 btn.addEventListener('click', (event) => {
-  event.target.textContent = "Loading";
+  event.target.textContent = "Loading...";
 
-  fetch(astrosUrl)
-    .then( respnse => respnse.json() )
-    .then(getProfiles)
-    .then( generateHTML )
-    .catch( err => {
-      peopleList.innerHTML = '<h3> Something went wrong. Check your Internet Connection...</h3>';
-      console.log(err);
+  getPeopleInSpace(astrosUrl)
+    .then(generateHTML)
+    .catch( e => {
+      peopleList.innerHTML = '<h3>Something went wrong!</h3>';
+      console.error(e);
     })
     .finally( () => event.target.remove() )
- 
 });
